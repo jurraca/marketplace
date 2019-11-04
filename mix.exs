b2655a1c15e70904d32f4e96c71930a34c2d93d3
ju <julienurraca@gmail.com>
2019-11-04 17:06:32 -0600

    remove gproc dependency

--- mix.exs
+++ mix.exs
@@ -21,7 +21,6 @@ defmodule Marketplace.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-       {:gproc, "~> 0.5.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
