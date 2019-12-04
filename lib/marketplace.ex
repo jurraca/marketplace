42ad03eccef36b09662ee3ebb27d2e85318df2eb
ju <julienurraca@gmail.com>
2019-12-04 15:33:34 -0600

    Update main module doc

--- lib/marketplace.ex
+++ lib/marketplace.ex
@@ -1,15 +1,17 @@
 defmodule Marketplace do
   @moduledoc """
-  Documentation for Marketplace.
+  Marketplace is an OTP application that provides the skeleton of a marketplace or trading engine. The impetus for building it was that simulating marketplace behavior is quite difficult, but thanks to BEAM process isolation, we can scale simulations while keeping individual Agents operating independently, that is, much closer to the reality of markets: independent actors, working in parallel at high volume.  
+
+  The feature set is limited, since every marketplace is different. (The main todo is a pubsub to guarantee data ordering). 
+
+  ### Usage 
+
+  `iex -S mix` will launch the application without simulators. You can use the defaults, or define your own simulators, and start them with `Supervisor.start_child()/2`. 
+   
+
+  ### Logging 
+  
+  By default, the application issues log messages to syslog using `ex_syslogger`. Feel free to silence this as the volume of log messages can grow rapidly.  
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Marketplace.hello()
-      :world
-
-  """
 end
