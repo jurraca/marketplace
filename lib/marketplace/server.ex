37917ca96d876b25973246dbd0615572d8bdd996
ju <julienurraca@gmail.com>
2020-08-16 16:01:18 -0500

    cleanup

--- lib/marketplace/server.ex
+++ lib/marketplace/server.ex
@@ -19,11 +19,10 @@ defmodule Marketplace.Server do
   @doc """
   Lists all orders in the marketplace, returns a list of `{order_name, state}`
   """
-
   def handle_call(:list, _from, _state) do
     list =
-      Registry.select(
-        Marketplace.Registry,
+      Marketplace.Registry
+      |> Registry.select(
         [
           {
             {:"$1", :_, :_},
@@ -32,21 +31,22 @@ defmodule Marketplace.Server do
           }
         ]
       )
+    |> Enum.map(fn x -> {x, Order.current_state(x)} end)
 
-    {:reply, Enum.map(list, fn x -> {x, Order.current_state(x)} end), nil}
+    {:reply, list, nil}
   end
 
   def handle_call({:get, id}, _from, _state) do
     case lk(id) do
       :ok -> {:reply, {id, Order.current_state(id)}, nil}
-      {:error, msg} -> {:reply, msg, nil}
+      nil -> {:reply, "no order exists", nil}
     end
   end
 
   def handle_call({:get, id, key}, _from, _state) do
     case lk(id) do
       :ok -> {:reply, {id, {key, Order.current_state(id, key)}}, nil}
-      {:error, msg} -> {:reply, msg, nil}
+      nil -> {:reply, nil, nil}
     end
   end
 
@@ -56,54 +56,49 @@ defmodule Marketplace.Server do
         :ok ->
           Order.post_order(key, value, id)
 
-        {:error, "No order by that name."} ->
+        nil ->
           {OrderSupervisor.start_child({id, key, value}), "No order exists. Creating one."}
-
-        {:error, "Order archived."} ->
-          {"Order was filled and archived."}
       end
 
-    {
-      :reply,
-      resp,
-      nil
-    }
+    {:reply, resp, nil}
   end
 
-  def handle_call({:match, key, value, id}, _from, _state) do
-    resp =
-      case lk(id) do
-        :ok -> valid_match?({id, key, value})
-        {:error, msg} -> msg
-      end
-
-    case resp do
-      true -> {:reply, Order.post_match(key, value, id), nil}
-      false -> {:reply, archive(id), nil}
-      msg -> {:reply, msg, nil}
+  @doc """
+  Attempt to match an order. Check whether it exists, whether the match is valid, and whether it succeeds.
+  """
+  def handle_call({:match, key, value, id} = opts, _from, _state) do
+    case lk(id) do
+      :ok -> valid_match?({id, key, value})
+      nil -> nil
     end
+    |> handle_match(opts)
   end
+
+  # Helpers
 
   defp lk(id), do: Registry.lookup(Marketplace.Registry, id) |> exists?(id)
 
-  # defp lk(id, key), do: Registry.lookup(Marketplace.Registry, id) |> lookup(id, key)
-
   defp exists?([{_pid, _} | _tail], _id), do: :ok
 
-  defp exists?([], _id), do: {:error, "No order by that name."}
+  defp exists?([], _id), do: nil
 
   @doc """
   Terminate and stop `Order` agent, log archive message.
   """
   def archive(id) do
-    Order.terminate(id)
-    :ok = Agent.stop(Order.via_tuple(id), :normal)
-    return = "Order archived."
-    Logger.info(return)
-    {:ok, return}
+    Order.terminate(id) # unregister the order from Registry
+    :ok = Agent.stop(Order.via_tuple(id), :normal) # Stop the agent.
+    Logger.info("Order archived.")
+    {:ok, "Order archived"}
   end
 
   def valid_match?({id, key, value}) do
     Order.current_state(id, key) - value > 0
   end
+
+  def handle_match(true, {key, value, id}), do: Order.post_match(key, value, id)
+
+  def handle_match(false, _opts), do: {:reply, "Invalid match, order has been filled.", nil}
+
+  def handle_match(nil, _), do: {:reply, "no order exists by that name", nil}
 end
